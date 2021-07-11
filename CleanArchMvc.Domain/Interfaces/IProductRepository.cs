40243179fd31577165ac7d35e087a5a7e96dae94
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-07-11 14:29:43 -0300

    Criados repositórios do domínio

---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetProductByCategoryAsync(int? id);
+        Task<Category> CreateAsync(Product category);
+        Task<Category> UpdateAsync(Product category);
+        Task<Category> RemoveAsync(Product category);
+    }
+}
