e0817d9c37200e99954382dcd4a2012d8da6b3eb
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-07-11 15:42:10 -0300

    Configuração inicial do banco de dados - Adicionados pacotes para uso do banco de dados - Criada classe de configuração do banco de dados

---
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
