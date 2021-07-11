b44dafd658774a88987a1d126db4e272b57d2d1b
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-07-11 13:49:07 -0300

    Criadas entidades do projeto como modelos anêmicos

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
