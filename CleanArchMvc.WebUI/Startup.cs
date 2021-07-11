d8a4c04ced118fb6b97e6479811d17886920b2e7
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-07-11 17:00:42 -0300

    Criado orquestrador para injeção de dependência
    - Criado injetor de dependências
    - Aplicado injetor na classe de startup da aplicação web

--- CleanArchMvc.WebUI/Startup.cs
+++ CleanArchMvc.WebUI/Startup.cs
@@ -1,13 +1,9 @@
+using CleanArchMvc.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI
 {
@@ -23,6 +19,7 @@ namespace CleanArchMvc.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddInfrastructure(Configuration);
             services.AddControllersWithViews();
         }
 
