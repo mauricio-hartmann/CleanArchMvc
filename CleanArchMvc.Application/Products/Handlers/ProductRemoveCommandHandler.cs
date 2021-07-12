140af6687c121df2a92bd3092617916cc813e0ca
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-07-12 19:52:08 -0300

    Criadas classes que implementam os comandos de persistência e consulta de produto

---
+++ CleanArchMvc.Application/Products/Handlers/ProductRemoveCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchMvc.Application.Products.Commands;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Products.Handlers
+{
+    class ProductRemoveCommandHandler : IRequestHandler<ProductRemoveCommand, Product>
+    {
+        private IProductRepository _productRepository;
+
+        public ProductRemoveCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Product> Handle(ProductRemoveCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+            if (product == null)
+                throw new ApplicationException("Entity could not be found");
+            return await _productRepository.RemoveAsync(product);
+        }
+    }
+}
---
+++ CleanArchMvc.Application/Products/Handlers/ProductUpdateCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchMvc.Application.Products.Commands;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Products.Handlers
+{
+    class ProductUpdateCommandHandler : IRequestHandler<ProductUpdateCommand, Product>
+    {
+        private IProductRepository _productRepository;
+
+        public ProductUpdateCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Product> Handle(ProductUpdateCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+            if (product == null)
+                throw new ApplicationException("Entity could not be found");
+            product.Update(request.Name, request.Description, request.Price, request.Stock, request.Image, request.CategoryId);
+            return await _productRepository.UpdateAsync(product);
+        }
+    }
+}
