a85a8cb613dec08defd99e0fbc6749c576c8c12e
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-07-15 15:33:09 -0300

    Implementada edição de categoria

--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -30,9 +31,38 @@ namespace CleanArchMvc.WebUI.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CategoryDTO category)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 await _categoryService.Add(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var category = await _categoryService.GetById(id);
+            if (category == null)
+                return NotFound();
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CategoryDTO category)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryService.Update(category);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
                 return RedirectToAction(nameof(Index));
             }
             return View(category);
